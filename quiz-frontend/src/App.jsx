de8580308d82868c3459bf24ca9f3e2509a2f4d2
Saurabh Shete <ss.saurabhshete@gmail.com>
2024-11-01 13:49:55 +0530

    feat: set up routing in App component for Main, Quiz, and Result pages

--- quiz-frontend/src/App.jsx
+++ quiz-frontend/src/App.jsx
@@ -1,29 +1,38 @@
-import React, { useRef } from 'react'
-import { useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { setUserId } from '../redux/result_reducer'
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-export default function App() {
-    const inputRef = useRef(null)
-    const dispatch = useDispatch()
+/** Import components */
+import Main from './components/Main'
+import Quiz from './components/Quiz'
+import Result from './components/Result'
+import { CheckUserExist } from './helper/helper'
 
-    function startQuiz() {
-        if (inputRef.current?.value) {
-            dispatch(setUserId(inputRef.current?.value))
-        }
-    }
-
-    return (
-        <div className="container mx-auto my-10 p-4">
-            <div className="flex justify-center items-center flex-col">
-                <div className="quiz bg-white flex justify-center items-center text-6xl font-bold text-red-500 rounded-full w-64 h-64">QUIZ</div>
-                <form id="form" className="mt-6 flex justify-center">
-                    <input ref={inputRef} className="p-3 border rounded-md w-1/2 text-center" type="text" placeholder="Username*" />
-                </form>
-                <div className="mt-6">
-                    <Link className="bg-red-500 text-white py-3 px-10 rounded-lg text-xl font-semibold" to={'/quiz'} onClick={startQuiz}>Start Quiz</Link>
-                </div>
-            </div>
-        </div>
+/** Define routes */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />
+  },
+  {
+    path: '/quiz',
+    element: (
+      <CheckUserExist>
+        <Quiz />
+      </CheckUserExist>
     )
+  },
+  {
+    path: '/result',
+    element: (
+      <CheckUserExist>
+        <Result />
+      </CheckUserExist>
+    )
+  }
+])
+
+function App() {
+  return <RouterProvider router={router} />
 }
+
+export default App
