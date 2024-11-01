959d9a821ab6587ea07c41a33cd48f7589c2c375
Saurabh Shete <ss.saurabhshete@gmail.com>
2024-11-01 13:30:37 +0530

    feat: add App component with TailwindCSS styling

--- quiz-frontend/src/App.jsx
+++ quiz-frontend/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { setUserId } from '../redux/result_reducer'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const inputRef = useRef(null)
+    const dispatch = useDispatch()
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    function startQuiz() {
+        if (inputRef.current?.value) {
+            dispatch(setUserId(inputRef.current?.value))
+        }
+    }
+
+    return (
+        <div className="container mx-auto my-10 p-4">
+            <div className="flex justify-center items-center flex-col">
+                <div className="quiz bg-white flex justify-center items-center text-6xl font-bold text-red-500 rounded-full w-64 h-64">QUIZ</div>
+                <form id="form" className="mt-6 flex justify-center">
+                    <input ref={inputRef} className="p-3 border rounded-md w-1/2 text-center" type="text" placeholder="Username*" />
+                </form>
+                <div className="mt-6">
+                    <Link className="bg-red-500 text-white py-3 px-10 rounded-lg text-xl font-semibold" to={'/quiz'} onClick={startQuiz}>Start Quiz</Link>
+                </div>
+            </div>
+        </div>
+    )
 }
-
-export default App
