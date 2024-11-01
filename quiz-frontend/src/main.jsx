2b2e3a17291f162bf47d0831f72af66aefdb6c1a
Saurabh Shete <ss.saurabhshete@gmail.com>
2024-11-01 13:50:19 +0530

    fix: ensure BrowserRouter wraps App for routing support

--- quiz-frontend/src/main.jsx
+++ quiz-frontend/src/main.jsx
@@ -4,9 +4,12 @@ import { Provider } from 'react-redux'
 import store from './redux/store'
 import App from './App'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </Provider>
 )
